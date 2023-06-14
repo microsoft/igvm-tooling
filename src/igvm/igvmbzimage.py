b39d189517b9126a0d4b34a09beb99b7e09fd936
Mahati Chamarthy <mahati.chamarthy@gmail.com>
2023-06-14 13:17:31 +0100

    Revert "igvm: Set the default value of rflags 2 (#19)"
    This reverts commit a01e99084a10c8d6696118cfa2a43740f3cb36b4.

--- src/igvm/igvmbzimage.py
+++ src/igvm/igvmbzimage.py
@@ -259,7 +259,6 @@ class IGVMLinuxGenerator(IGVMBaseGenerator):
         self.state.vmsa.rip = kernel_entry
         self.state.vmsa.rsi = boot_params_addr
         self.state.vmsa.rsp = boot_stack_addr + PGSIZE
-        self.state.vmsa.rflags = 2
 
 
 class IGVMLinux2Generator(IGVMLinuxGenerator):
